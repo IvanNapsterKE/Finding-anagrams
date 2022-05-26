d7bf18760184aa643b28696034b600a2ce7f2787
IvanNapsterKE <nguyoivan@gmail.com>
2022-05-26 13:36:24 +0300

    main

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 def finding_anagrams(word):
 
-    str1 = input("First word: ")
-    str2 = input("Second word: ")
+# Type first word and second word
 
-    return sorted(str1) == sorted(str2)
+    str1 = input("First word: ").lower()
+    str2 = input("Second word: ").lower()
 
+    
+    for char in str1:
+        
+        if char not in str2:
+            
+            return False
+    return True
+
+#check length of words
+    if len(str1) != len(str2()):
+        
+        return  False
+    return True
+
+  
 
 print(finding_anagrams("word"))
